76eed14d87b348b8d0f568d8807680215274d938
Daniel <daniel-matuch@wp.pl>
2025-12-21 15:00:58 +0100

    Rozwiązanie konfliktów

--- routes/quiz-learn.js
+++ routes/quiz-learn.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
